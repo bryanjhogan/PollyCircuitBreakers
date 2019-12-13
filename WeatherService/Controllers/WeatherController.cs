741e2ab7092feaaf91368804a9386fd140f0fbc3
Bryan Hogan <bryanjhogan@outlook.com>
2019-12-12 22:52:14 -0500

    beginning code

--- WeatherService/Controllers/WeatherController.cs
+++ WeatherService/Controllers/WeatherController.cs
@@ -7,42 +7,6 @@ namespace WeatherService.Controllers
     [Route("[controller]")]
     public class WeatherController : ControllerBase
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-
-        public WeatherController(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
-
-        [HttpGet("fahrenheit/{locationId}")]
-        public async Task<ActionResult> GetFahrenheit(int locationId)
-        {
-            var httpClient = _httpClientFactory.CreateClient("TemperatureService");
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"fahrenheit/{locationId}");
-
-            if (httpResponseMessage.IsSuccessStatusCode)
-            {
-                int temperature = await httpResponseMessage.Content.ReadAsAsync<int>();
-                return Ok(temperature);
-            }
-
-            return StatusCode((int)httpResponseMessage.StatusCode, await httpResponseMessage.Content.ReadAsStringAsync());
-        }
-
-        [HttpGet("celsius/{locationId}")]
-        public async Task<ActionResult> GetCelsius(int locationId)
-        {
-            var httpClient = _httpClientFactory.CreateClient("TemperatureService");
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"celsius/{locationId}");
-
-            if (httpResponseMessage.IsSuccessStatusCode)
-            {
-                int temperature = await httpResponseMessage.Content.ReadAsAsync<int>();
-                return Ok(temperature);
-            }
-
-            return StatusCode((int)httpResponseMessage.StatusCode, await httpResponseMessage.Content.ReadAsStringAsync());
-        }
 
     }
 }
--- WeatherService/Startup.cs
+++ WeatherService/Startup.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Polly;
-using System;
-using System.Net.Http;
 
 namespace WeatherService
 {
@@ -20,37 +21,7 @@ namespace WeatherService
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var basicCircuitBreakerPolicy = Policy
-                .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
-                .CircuitBreakerAsync(2, TimeSpan.FromSeconds(30), OnBreak, OnReset, OnHalfOpen);
-
-            var advancedCircuitBreakerPolicy = Policy
-                .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
-                .AdvancedCircuitBreakerAsync(0.25, TimeSpan.FromSeconds(60), 7, TimeSpan.FromSeconds(30),
-                    OnBreak, OnReset, OnHalfOpen);
-
-            services.AddHttpClient("TemperatureService", client =>
-            {
-                client.BaseAddress = new Uri("http://localhost:6001/");
-                client.DefaultRequestHeaders.Add("Accept", "application/json");
-            }).AddPolicyHandler(basicCircuitBreakerPolicy);
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
-
-        private void OnHalfOpen()
-        {
-            Console.WriteLine("Circuit in test mode, one request will be allowed.");
-        }
-
-        private void OnReset()
-        {
-            Console.WriteLine("Circuit closed, requests flow normally.");
-        }
-
-        private void OnBreak(DelegateResult<HttpResponseMessage> result, TimeSpan ts)
-        {
-            Console.WriteLine("Circuit cut, requests will not flow.");
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
